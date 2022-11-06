c81178f9b6e4209d345efec5fea54b40406d2772
kulotsystems <kulotsystems@gmail.com>
2022-11-06 13:30:07 +0800

    Install other dependencies.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,37 @@
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// plugins
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
+
+// utilities
+import { defineConfig, loadEnv } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 
 export default ({ mode }) => {
   const config = {
-    plugins: [vue()]
+    plugins: [
+        vue(),
+        // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+        vuetify({
+          autoImport: true,
+        }),
+    ],
+    resolve: {
+      alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+      extensions: [
+          '.js',
+          '.json',
+          '.jsx',
+          '.mjs',
+          '.ts',
+          '.tsx',
+          '.vue',
+      ],
+    },
+    server: {
+      port: 3000,
+    },
   };
 
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
