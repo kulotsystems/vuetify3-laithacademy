c0a9383fbf36c0fee1b7de58189aaa441cde9bee
kulotsystems <kulotsystems@gmail.com>
2022-11-06 12:35:45 +0800

    Update vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ mode }) => {
+  const config = {
+    plugins: [vue()]
+  };
+
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  const SKIP_BASE_PATH = process.env.VITE_SKIP_BASE_BATH;
+  if (SKIP_BASE_PATH === undefined || SKIP_BASE_PATH === 'false')
+    config.base = '/vuetify3-laithacademy/';
+
+  return defineConfig(config);
+}
